dd617a889967673f11fddc2407890167aeb3e984
Williams <61099757+Williethedeveloper@users.noreply.github.com>
2026-07-01 01:32:10 +0100

    Add initial C++ program for assignment 1

---
+++ lab1/assignment1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+    using namespace std;
+    int main() {
+        cout << "Test Student" << endl;
+        cout << "Department of Computer Engineering" << endl;
+        return 0;
+    }
